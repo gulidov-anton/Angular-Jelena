8b6cf2d95f182cd8f9b2a278fa821151a12f85cb
Anton <gulidov.antonio@google.com>
2020-11-08 22:04:36 +0300

    changed some code

--- 7/Constructor1/script.js
+++ 7/Constructor1/script.js
@@ -13,16 +13,16 @@ mainTextAddN.onclick = function(){
     mainText.style.opacity(0.5);
 };
 
-function getData(){
+function getData() {
     let newHTML;
     //приём данных из формы
-    let title = title.value;// название
-    let description = description.value;// слоган
-    let menu = menu.value;// меню навигации
-    let LogoImg = logoImg.value;// логотип
-    let mainText = mainText.value;// основной текст
-    let colorTheme = colorTheme.value;// цветовая тема
-    let colorText = colorText.value;// цвет текста
+    let title = document.getElementById('title').value; // название
+    let description = document.getElementById('description').value;// слоган
+    let menu = document.getElementsByName('menu').value;// меню навигации
+    let LogoImg = document.getElementById('logoImg').value;// логотип
+    let mainText = document.getElementsByName('mainTextAdd').value;// основной текст
+    let colorTheme = document.getElementById('colorTheme').value;// цветовая тема
+    let colorText = document.getElementById('colorText').value;// цвет текста
 
     document.body.style.backgroundColor = colorTheme;
 
@@ -32,7 +32,7 @@ function getData(){
     newHTML += `<h2>${description}</h2></header>`;
 
     //создание строки пунктов меню
-    let list = document.constructor.list;
+    let list = document.getElementsByName('list');
     let listItems = [];
     let li = '';
     
@@ -48,16 +48,16 @@ function getData(){
     //выбор шаблона
     switch(menu) {
         case 'horizontal':
-            menuItem = `<section class="finalPlacementVertical"><nav id="navHorizontal">${ul}</nav>`;
+            menuItem = `<section class="finalPlacementVertical"><nav id="navHorizontal">${menu}</nav>`;
             break;
         case 'verticalLeft':
-            menuItem = `<section class="finalPlacementLeft"><nav id="navVerticalLeft">${ul}</nav>`;
+            menuItem = `<section class="finalPlacementLeft"><nav id="navVerticalLeft">${menu}</nav>`;
             break;
         case 'verticalRight':
-            menuItem = `<section class="finalPlacementRight"><nav id="navVerticalRight">${ul}</nav>`;
+            menuItem = `<section class="finalPlacementRight"><nav id="navVerticalRight">${menu}</nav>`;
             break;
         default:
-            menuItem = `<section class="finalPlacement"><nav id="navHorizontal">${ul}</nav>`;
+            menuItem = `<section class="finalPlacement"><nav id="navHorizontal">${menu}</nav>`;
     };
     newHTML += menuItem;
 
